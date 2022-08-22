634442843c8327da95475564d14e79d5a73f7e61
Mohit Negi <72944509+MohitNegi19@users.noreply.github.com>
2022-08-22 21:59:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,11 @@ const nextConfig = {
     domains: ["www.iconsdb.com","rb.gy"]
   }
 }
-
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 module.exports = nextConfig
